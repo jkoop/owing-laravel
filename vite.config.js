21c40d6cdaf923c653f2f2375310367677c3ab9c
Joe Koop <joe@joekoop.com>
2024-01-13 21:30:08 +0000

    someone's gonna need to format this

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import laravel from "laravel-vite-plugin"
 export default defineConfig({
 	plugins: [
 		laravel({
-			input: ["resources/css/app.css", "resources/js/app.js"],
+			input: [
+"resources/js/successes.js",
+"resources/js/bootstrap.js",
+"resources/js/app.js",
+"resources/js/datetime-relative.js",
+"resources/js/datetime-absolute.js",
+"resources/css/successes.css",
+"resources/css/change-history.css",
+"resources/css/app.css",
+"resources/css/errors.css",
+"resources/css/input.css",
+            ],
 			refresh: true,
 		}),
 	],
